45986bc902c541fb8e930d519a68ac2459ec973e
HenriqueContini <henrique.coutine@gmail.com>
2023-11-13 09:51:02 -0300

    Feat: Adding Docker

--- backEnd/src/database/data-source.ts
+++ backEnd/src/database/data-source.ts
@@ -1,24 +1,16 @@
 import { DataSource } from "typeorm";
 import { Product } from "../entity/Product";
 import { Pack } from "../entity/Pack";
-import { CreateTables1693951908092 } from "./migrations/1693951908092-createTables";
-import { InsertDataIntoProducts1693953158470 } from "./migrations/1693953158470-insertDataIntoProducts";
-import { InsertDataIntoPacks1693955422100 } from "./migrations/1693955422100-insertDataIntoPacks";
 import "dotenv/config";
 
 export const AppDataSource = new DataSource({
   type: "mysql",
   host: process.env.MYSQL_HOST,
-  port: Number(process.env.MYSQL_LOCAL_PORT),
+  port: Number(process.env.MYSQL_DOCKER_PORT),
   username: process.env.MYSQL_USERNAME,
   password: process.env.MYSQL_PASSWORD,
-  database: "price_updater",
+  database: process.env.MYSQL_DATABASE,
   synchronize: false,
   logging: false,
   entities: [Product, Pack],
-  migrations: [
-    CreateTables1693951908092,
-    InsertDataIntoProducts1693953158470,
-    InsertDataIntoPacks1693955422100,
-  ],
 });
--- backEnd/src/server.ts
+++ backEnd/src/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const PORT: number = 3000;
+const PORT: number = 3000 || process.env.NODE_DOCKER_PORT;
 
 app.listen(PORT, () => {
   console.log(`Running on: http://localhost:${PORT}`);
